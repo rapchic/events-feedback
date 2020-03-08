9c22c079395ef199e22298e718d9642878691012
Debjani Chatterjee <debjani93@in.ibm.com>
2020-03-08 19:37:14 +0530

    changed host info

--- app.js
+++ app.js
@@ -171,3 +171,5 @@ var port = process.env.PORT || 3000
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
+
+module.exports = app;
